6987346a12c85c5111b2dda50e639417d12b9884
bebe200 <tranlethanhminh@gmail.com>
2024-04-21 11:21:29 +0900

    Create session

--- Source/EOSFun/EOSGameInstance.cpp
+++ Source/EOSFun/EOSGameInstance.cpp
@@ -4,6 +4,9 @@
 #include "EOSGameInstance.h"
 #include "OnlineSubsystem.h"
 #include "Interfaces/OnlineIdentityInterface.h"
+#include "Interfaces/OnlineSessionInterface.h"
+#include "OnlineSessionSettings.h"
+#include "Kismet/GameplayStatics.h"
 
 void UEOSGameInstance::Login()
 {
@@ -18,23 +21,78 @@ void UEOSGameInstance::Login()
 	}
 }
 
+void UEOSGameInstance::CreateSession()
+{
+	if (sessionPtr)
+	{
+		FOnlineSessionSettings sessionSettings;
+		sessionSettings.bAllowInvites = true;
+		sessionSettings.bIsDedicated = false;
+		sessionSettings.bIsLANMatch = false;
+		sessionSettings.bShouldAdvertise = true;
+		sessionSettings.bUseLobbiesIfAvailable = true;
+		sessionSettings.bUsesPresence = true;
+		sessionSettings.bAllowJoinInProgress = true;
+		sessionSettings.bAllowJoinViaPresence = true;
+		sessionSettings.NumPublicConnections = true;
+
+		//passing an abitray data, and make is avaliable to be read on the client.
+		sessionSettings.Set(FName("LobbyName"), FString("MyFunLobby"), EOnlineDataAdvertisementType::ViaOnlineServiceAndPing);
+
+		sessionPtr->CreateSession(0, "FunSession", sessionSettings);
+	}
+}
+
+void UEOSGameInstance::LoginCompletedDelegate(FLoginCompletedDelegate loginCompletedDelegate)
+{
+	OnLoginCompletedDelegate = loginCompletedDelegate;
+}
+
 void UEOSGameInstance::Init()
 {
 	Super::Init();
 
-	identityPtr = IOnlineSubsystem::Get()->GetIdentityInterface();
+	onlineSubsystem = IOnlineSubsystem::Get();
 
+	identityPtr = onlineSubsystem->GetIdentityInterface();
 	identityPtr->OnLoginCompleteDelegates->AddUObject(this, &UEOSGameInstance::OnLoginCompleted);
+
+	sessionPtr = onlineSubsystem->GetSessionInterface();
+	sessionPtr->OnCreateSessionCompleteDelegates.AddUObject(this, &UEOSGameInstance::OnCreateSessionCompleted);
 }
 
 void UEOSGameInstance::OnLoginCompleted(int numOfPlayers, bool bWasSuccessful, const FUniqueNetId& UserId, const FString& Error)
 {
 	if (bWasSuccessful)
 	{
-		UE_LOG(LogTemp, Warning, TEXT("Login Successed"))
+		UE_LOG(LogTemp, Warning, TEXT("Login Successed"));
 	}
 	else
 	{
-		UE_LOG(LogTemp, Warning, TEXT("Login failed: %s"), *Error)
+		UE_LOG(LogTemp, Warning, TEXT("Login failed: %s"), *Error);
+	}
+
+	OnLoginCompletedDelegate.ExecuteIfBound(numOfPlayers, bWasSuccessful);
+}
+
+void UEOSGameInstance::OnCreateSessionCompleted(FName name, bool bWasSuccessful)
+{
+	if (bWasSuccessful)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Session created Successfully"))
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Session createtion failed"))
+	}
+	
+	if (!LobbyLevel.IsValid())
+	{
+		LobbyLevel.LoadSynchronous();
+	}
+	if (LobbyLevel.IsValid())
+	{
+		const FName LevelName = FName(*FPackageName::ObjectPathToPackageName(LobbyLevel.ToString()));
+		GetWorld()->ServerTravel(LevelName.ToString() + "?listen");
 	}
 }
--- Source/EOSFun/EOSGameInstance.h
+++ Source/EOSFun/EOSGameInstance.h
@@ -7,19 +7,53 @@
 #include "EOSGameInstance.generated.h"
 
 /**
- * 
+ *
  */
+
+DECLARE_DYNAMIC_DELEGATE_TwoParams(FLoginCompletedDelegate, int, numOfPlayers, bool, bWasSuccessful);
+
+
+class IOnlineSubsystem;
+
 UCLASS()
 class EOSFUN_API UEOSGameInstance : public UGameInstance
 {
 	GENERATED_BODY()
-public:
+
+public: // Functions 
+
 	UFUNCTION(BlueprintCallable)
 	void Login();
-protected:
+
+	UFUNCTION(BlueprintCallable)
+	void CreateSession();
+
+	UFUNCTION(BlueprintCallable)
+	void LoginCompletedDelegate(FLoginCompletedDelegate loginCompletedDelegate);
+
+
+public: // Variables
+
+	UPROPERTY(EditDefaultsOnly, Category = "Write")
+	TSoftObjectPtr<UWorld> LobbyLevel;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "Read")
+	bool bLoginSuccess = false;
+
+	UPROPERTY()
+	FLoginCompletedDelegate OnLoginCompletedDelegate;
+
+protected: // Functions
 	virtual void Init() override;
-private:
+
+private: // Variables
+
+	IOnlineSubsystem* onlineSubsystem;
 
 	TSharedPtr<class IOnlineIdentity, ESPMode::ThreadSafe> identityPtr;
+	TSharedPtr<class IOnlineSession, ESPMode::ThreadSafe> sessionPtr;
+
+private : // Functions
 	void OnLoginCompleted(int numOfPlayers, bool bWasSuccessful, const FUniqueNetId& UserId, const FString& Error);
+	void OnCreateSessionCompleted(FName name, bool bWasSuccessful);
 };
